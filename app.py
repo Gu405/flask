728f64fbb03bd8f70365b9224f3535350759b150
thanh dat <datdo.230901@gmail.com>
2021-02-10 16:01:26 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+def home():
+    techs = ['HTML', 'CSS', 'Flask', 'Python']
+    name = '30 days of Python Programing'
+    return render_template('home.html', techs=techs, name = name, title='Home')
+
+@app.route('/about')
+def about():
+    name = '30 days of Python Programming'
+    return render_template('about.html', name = name, title = 'About us')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    name = 'Text Analyzer'
+    if request.method == 'GET':
+        return render_template('post.html', name=name, title=name)
+    if request.method == 'POST':
+        content = request.form['content']
+        print(content)
+        return redirect(url_for('result'))
+    return render_template('post.html', name = name, title= name)
+if __name__ == '__main__':
+    # for deployment we use the environ
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='127.0.0.1', port=port)
